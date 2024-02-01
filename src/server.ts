0cca1b80ff798f88036b952759e8e8eb1b494d05
Austin Noronha <austin_noronha@hotmail.com>
2024-02-01 13:20:22 +0530

    Added handlebars with layouts

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 // src/index.js
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import path from "path";
+import {default as hbs} from "hbs";
 import { homeRoute } from "./routes/home";
 import { usersRoute } from "./routes/users";
 
@@ -9,8 +11,13 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.use( homeRoute );
-app.use( usersRoute );
+//Sets our app to use the handlebars engine
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view options', { layout: 'layouts/main' });
+
+app.use(homeRoute);
+app.use(usersRoute);
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
